4dd65f3316350b26be77791b2f5e0f10ce4499bf
hppRC <hpp.ricecake@gmail.com>
2019-08-01 17:54:28 +0900

    add saveImage, deleteImage, and deleteKey

--- app/Services/ApiShopService.php
+++ app/Services/ApiShopService.php
@@ -8,16 +8,18 @@ class ApiShopService
 {
     public $addRule = [
         'name' => 'required|max:100',
+        'image' => 'required|image|max:10240'
     ];
 
     public $updateRule = [
         'name' => 'filled|max:100',
+        'image' => 'required|image|max:10240'
     ];
 
 
     public function getShops()
     {
-        return Shop::query()->select(['id', 'name'])->get();
+        return Shop::query()->select(['id', 'name','imageUrl'])->get();
     }
 
     public function addShop($shop_params)
@@ -40,5 +42,34 @@ class ApiShopService
     {
         $shop = Shop::find($shop_id);
         $shop->delete();
+    }
+
+    public function saveImageReturnUrl($image)
+    {
+        $contents = file_get_contents($image->getRealPath());
+        $disk = Storage::disk('s3');
+        $imageName = Carbon::now().$image->hashName();
+        $disk->put($imageName, $contents,'public');
+        $imageUrl = $disk->url($imageName);
+        return $imageUrl;
+    }
+
+
+    public function deleteImage($imageUrl)
+    {
+        $disk = Storage::disk('s3');
+        $imageNameEncoded = pathinfo($imageUrl)['basename'];
+        $imageName = urldecode($imageNameEncoded);
+        $disk->delete($imageName);
+    }
+
+    public function deleteKeyHasNothing($shop_params)
+    {
+        foreach (array_keys($shop_params) as $key) {
+            if (empty($shop_params[$key])) {
+                unset($shop_params[$key]);
+            }
+        }
+        return $shop_params;
     }
 }
